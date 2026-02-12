52bd939a54f70beec3b52c81e8be24fb2f712e8c
wangxl <wangty334752@gmail.com>
2026-02-12 09:59:26 +0800

    feat: langchain4j 支持 MCP

---
+++ langchain4j-14-chat-mcp/src/main/java/com/bubua12/langchain4j/mcp/LangChainMCPApplication.java
@@ -0,0 +1,17 @@
+package com.bubua12.langchain4j.mcp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *
+ *
+ * @author bubua12
+ * @since 2026/2/12 9:52
+ */
+@SpringBootApplication
+public class LangChainMCPApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LangChainMCPApplication.class, args);
+    }
+}
